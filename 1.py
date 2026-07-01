2ca39524e3e2ea6736f8218b608703af358f44c2
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-07-01 13:17:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,45 @@
+##Single line comments
+##we use this
+#this is also for comments
+
+'''
+for mlti line comments we use 
+tripple quotes
+'''
+
+
+#python is case sensitive
+
+name = "Pri"
+Name = "Jai" 
+
+#both these variables are 2 diff variables
+print(name)
+print(Name)
+
+
+#Indentation is important
+
+age = 32
+if age>30:
+  print(age)
+print(age)
+
+age1 = 22
+if age1>30:
+  print(age1)
+print(age1)
+
+
+#Line continuation
+
+total = 1+2+3+4+5+6+7+\
+4+5+6
+
+print(total)
+
+
+#Multiple Statements on a single line
+x = 5; y = 10; z = x+y
+print(z)
+
